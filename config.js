524f1ec9ca140e473fafd7037585eb0df7fbd4f2
mocrelacje-spec <mocrelacje@gmail.com>
2025-09-08 23:27:55 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+/* config.js - poprawiona wersja do biegu na orientację */
+
+// Wklej tutaj adres swojego wdrożonego Google Apps Script (link kończący się na /exec)
+window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwfO0zjbwZzUKVxJMGhJ4CZE8yTyeN-EaZ3zU0kX8uPTUMYgT30S4utmukBhMA1PqV9/exec";
+
+// Wklej adres, pod którym będą działały Twoje strony (np. hosting, GitHub Pages itp.)
+window.APP_BASE_URL  = "https://github.com/mocrelacje-spec/BnO.git";
