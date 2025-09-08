a634f374004781455593ef63c70ddaef6ae35b2d
mocrelacje-spec <mocrelacje@gmail.com>
2025-09-09 00:45:50 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -3,4 +3,4 @@
 // Wklej tutaj adres swojego wdrożonego Google Apps Script (link kończący się na /exec)
 window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwxnc-ur_zkVLF_kG48nCxYu9djO6AeLc9YB1BeTIqsup-sf77YpdlbhJwCPR8axm8x/exec"
 // Wklej adres, pod którym będą działały Twoje strony (np. hosting, GitHub Pages itp.)
-window.APP_BASE_URL  = "https://utn.pl/lGmJw/index.html";
+window.APP_BASE_URL  = "https://bno-zcemip.netlify.app/index.html";
