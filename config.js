647b16eb8b4c4522ffa2bf906fc4b91b586fb34e
mocrelacje-spec <mocrelacje@gmail.com>
2025-09-09 00:26:00 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 /* config.js - poprawiona wersja do biegu na orientację */
 
 // Wklej tutaj adres swojego wdrożonego Google Apps Script (link kończący się na /exec)
-window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwfO0zjbwZzUKVxJMGhJ4CZE8yTyeN-EaZ3zU0kX8uPTUMYgT30S4utmukBhMA1PqV9/exec";
-
+window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwxnc-ur_zkVLF_kG48nCxYu9djO6AeLc9YB1BeTIqsup-sf77YpdlbhJwCPR8axm8x/exec
 // Wklej adres, pod którym będą działały Twoje strony (np. hosting, GitHub Pages itp.)
 window.APP_BASE_URL  = "https://utn.pl/lGmJw/index.html";
